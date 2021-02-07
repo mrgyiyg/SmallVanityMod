d14e948e08e8305a687329ca40c4863c2e8b7c69
gyiyg "Dr. Ant, MD" yourselfcola <78702698+mrgyiyg@users.noreply.github.com>
2021-02-07 15:12:41 -0500

    Create AntlionMask.cs

---
+++ Items/AntlionMask.cs
@@ -0,0 +1,33 @@
+using Terraria.ModLoader;
+using Terraria.ID;
+
+namespace SmallVanityProject.Items
+{
+	[AutoloadEquip(EquipType.Head)]
+	public class AntlionMask : ModItem
+	{
+		public override void SetStaticDefaults() {
+			base.SetStaticDefaults();
+			DisplayName.SetDefault("Antlion Mask");
+			}
+			
+		public override void SetDefaults() {
+			item.width = 28;
+			item.height = 26;
+			item.rare = ItemRarityID.Blue;
+			item.vanity = true;
+		}
+
+		public override bool DrawHead() {
+			return false;
+		}
+		
+		public override void AddRecipes() {
+			ModRecipe recipe = new ModRecipe(mod);
+			recipe.AddIngredient(ItemID.AntlionMandible, 2);			
+			recipe.AddTile(TileID.WorkBenches);
+			recipe.SetResult(this);
+			recipe.AddRecipe();
+		}
+	}
+}
