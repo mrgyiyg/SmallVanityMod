c88a5c3e3c976962c27a1d9879df8551c1040991
gyiyg "Dr. Ant, MD" yourselfcola <78702698+mrgyiyg@users.noreply.github.com>
2021-02-09 17:42:13 -0500

    Add files via upload

---
+++ Items/FunnyMouth.cs
@@ -0,0 +1,38 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+using Microsoft.Xna.Framework;
+
+namespace SmallVanityProject.Items
+{
+	[AutoloadEquip(EquipType.Face)]
+	class FunnyMouth : ModItem
+	{
+
+		public override void SetStaticDefaults() {
+			base.SetStaticDefaults();
+			DisplayName.SetDefault("Funny Mouth");
+			Tooltip.SetDefault("Are you a bad enough dude to kiss me with tongue?");
+			}
+
+		public override void SetDefaults()
+		{
+			item.accessory = true;
+			item.width = 18;
+			item.height = 18;
+			item.rare = ItemRarityID.Blue;
+			item.vanity = true;
+			item.value = Item.sellPrice(0, 1, 50, 0);
+		}
+		
+		public override void AddRecipes() {
+			ModRecipe recipe = new ModRecipe(mod);
+			recipe.AddIngredient(ItemID.TissueSample, 2);			
+			recipe.AddTile(TileID.LunarMonolith);
+			recipe.SetResult(this);
+			recipe.AddRecipe();
+		}
+		
+	}
+}
