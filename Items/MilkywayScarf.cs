d78015a4feff41c38cfcf492b680f5c8c674d73e
gyiyg "Dr. Ant, MD" yourselfcola <78702698+mrgyiyg@users.noreply.github.com>
2021-02-09 23:25:08 -0500

    Add files via upload

---
+++ Items/MilkywayScarf.cs
@@ -0,0 +1,59 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace SmallVanityProject.Items
+{
+	[AutoloadEquip(EquipType.Neck)]
+	class MilkywayScarf : ModItem
+	{
+
+		public override void SetStaticDefaults() {
+			base.SetStaticDefaults();
+			DisplayName.SetDefault("Milkyway Scarf");
+			}
+
+		public override void SetDefaults()
+		{
+			item.accessory = true;
+			item.width = 28;
+			item.height = 28;
+			item.rare = ItemRarityID.Blue;
+			item.vanity = true;
+			item.value = Item.sellPrice(0, 2, 50, 0);
+		}
+		
+		public override void PostDrawInWorld(SpriteBatch spriteBatch, Color lightColor, Color alphaColor, float rotation, float  scale, int whoAmI) 	
+{
+	Texture2D texture = mod.GetTexture("MilkywayScarf_Glow");
+	spriteBatch.Draw
+	(
+		texture,
+		new Vector2
+		(
+			item.position.X - Main.screenPosition.X + item.width * 0.5f,
+			item.position.Y - Main.screenPosition.Y + item.height - texture.Height * 0.5f + 2f
+		),
+		new Rectangle(0, 0, texture.Width, texture.Height),
+		Color.White,
+		rotation,
+		texture.Size() * 0.5f,
+		scale, 
+		SpriteEffects.None, 
+		0f
+	);
+}
+	
+	        public override void ArmorSetShadows(Player player)
+        {
+            player.armorEffectDrawShadow = true;
+
+           
+            //Lighting.AddLight(player.Center, .2f, .2f, .2f);
+        }
+		
+	}
+}
