3b646a63b094c7084dc7800677cb98ae5a52896c
gyiyg "Dr. Ant, MD" yourselfcola <78702698+mrgyiyg@users.noreply.github.com>
2021-02-10 17:18:51 -0500

    Update TwinklePopperMask.cs

--- Items/TwinklePopperMask.cs
+++ Items/TwinklePopperMask.cs
@@ -3,6 +3,7 @@ using Terraria.ID;
 using Terraria.ModLoader;
 using static Terraria.ModLoader.ModContent;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace SmallVanityProject.Items
 {
@@ -25,6 +26,27 @@ namespace SmallVanityProject.Items
 		public override bool DrawHead() {
 			return false;
 		}
+		
+				public override void PostDrawInWorld(SpriteBatch spriteBatch, Color lightColor, Color alphaColor, float rotation, float  scale, int whoAmI) 	
+{
+	Texture2D texture = mod.GetTexture("Items/Glow/TwinklePopperMask_Glow");
+	spriteBatch.Draw
+	(
+		texture,
+		new Vector2
+		(
+			item.position.X - Main.screenPosition.X + item.width * 0.5f,
+			item.position.Y - Main.screenPosition.Y + item.height - texture.Height * 0.5f + 2f
+		),
+		new Rectangle(0, 0, texture.Width, texture.Height),
+		Color.White,
+		rotation,
+		texture.Size() * 0.5f,
+		scale, 
+		SpriteEffects.None, 
+		0f
+	);
+}
 		
 	}
 }
