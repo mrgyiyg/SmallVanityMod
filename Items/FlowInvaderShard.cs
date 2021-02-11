a9ebb0708dedded7b97ebbfae125cf594930b3f8
gyiyg "Dr. Ant, MD" yourselfcola <78702698+mrgyiyg@users.noreply.github.com>
2021-02-10 20:48:10 -0500

    Update FlowInvaderShard.cs

--- Items/FlowInvaderShard.cs
+++ Items/FlowInvaderShard.cs
@@ -3,6 +3,7 @@ using Terraria.ID;
 using Terraria.ModLoader;
 using static Terraria.ModLoader.ModContent;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace SmallVanityProject.Items
 {
@@ -27,5 +28,26 @@ namespace SmallVanityProject.Items
 				player.AddBuff(item.buffType, 3600, true);
 			}
 		}
+		
+		public override void PostDrawInWorld(SpriteBatch spriteBatch, Color lightColor, Color alphaColor, float rotation, float  scale, int whoAmI) 	
+{
+	Texture2D texture = mod.GetTexture("Items/Glow/FlowInvaderShard_Glow");
+	spriteBatch.Draw
+	(
+		texture,
+		new Vector2
+		(
+			item.position.X - Main.screenPosition.X + item.width * 0.5f,
+			item.position.Y - Main.screenPosition.Y + item.height - texture.Height * 0.5f + 2f
+		),
+		new Rectangle(0, 0, texture.Width, texture.Height),
+		Color.White,
+		rotation,
+		texture.Size() * 0.5f,
+		scale, 
+		SpriteEffects.None, 
+		0f
+	);
+}
 	}
 }
