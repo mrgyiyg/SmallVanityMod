c21475c9707de2f80ac4a3b9a686d0aa942ffeae
gyiyg "Dr. Ant, MD" yourselfcola <78702698+mrgyiyg@users.noreply.github.com>
2021-02-12 00:13:49 -0500

    Add files via upload

---
+++ Items/RedPumphead.cs
@@ -0,0 +1,38 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+using Microsoft.Xna.Framework;
+
+namespace SmallVanityProject.Items
+{
+	[AutoloadEquip(EquipType.Head)]
+	public class RedPumphead : ModItem
+	{
+		public override void SetStaticDefaults() {
+			base.SetStaticDefaults();
+			DisplayName.SetDefault("Red Pumpkinhead");
+			}
+			
+		public override void SetDefaults() {
+			item.width = 28;
+			item.height = 28;
+			item.rare = ItemRarityID.Blue;
+			item.vanity = true;
+			item.value = Item.sellPrice(0, 1, 50, 0);
+		}
+
+		public override bool DrawHead() {
+			return false;
+		}
+		
+		public override void AddRecipes() {
+			ModRecipe recipe = new ModRecipe(mod);
+			recipe.AddIngredient(ItemID.Pumpkin, 1);
+			recipe.AddIngredient(ItemID.RedDye, 1);			
+			recipe.AddTile(TileID.WorkBenches);
+			recipe.SetResult(this);
+			recipe.AddRecipe();
+		}
+	}
+}
