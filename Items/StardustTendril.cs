4d729f6ce1f1b281c8d7bce042c2f2e8ac8c07d1
gyiyg "Dr. Ant, MD" yourselfcola <78702698+mrgyiyg@users.noreply.github.com>
2021-02-13 21:08:06 -0500

    Add files via upload

---
+++ Items/StardustTendril.cs
@@ -0,0 +1,61 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace SmallVanityProject.Items
+{
+	[AutoloadEquip(EquipType.Balloon)]
+	class StardustTendril : ModItem
+	{
+
+		public override void SetStaticDefaults() {
+			base.SetStaticDefaults();
+			DisplayName.SetDefault("Writhing Stardust Tendril");
+			Tooltip.SetDefault("Go ahead, touch it. You know you want to");
+			}
+
+		public override void SetDefaults()
+		{
+			item.accessory = true;
+			item.width = 30;
+			item.height = 34;
+			item.rare = ItemRarityID.Blue;
+			item.vanity = true;
+			item.value = Item.sellPrice(0, 2, 50, 0);
+		}
+		
+		
+		public override void PostDrawInWorld(SpriteBatch spriteBatch, Color lightColor, Color alphaColor, float rotation, float  scale, int whoAmI) 	
+{
+	Texture2D texture = mod.GetTexture("Items/Glow/StardustTendril_Glow");
+	spriteBatch.Draw
+	(
+		texture,
+		new Vector2
+		(
+			item.position.X - Main.screenPosition.X + item.width * 0.5f,
+			item.position.Y - Main.screenPosition.Y + item.height - texture.Height * 0.5f + 2f
+		),
+		new Rectangle(0, 0, texture.Width, texture.Height),
+		Color.White,
+		rotation,
+		texture.Size() * 0.5f,
+		scale, 
+		SpriteEffects.None, 
+		0f
+	);
+}
+
+		public override void AddRecipes() {
+			ModRecipe recipe = new ModRecipe(mod);
+			recipe.AddIngredient(ItemID.FragmentStardust, 10);			
+			recipe.AddTile(TileID.LunarMonolith);
+			recipe.SetResult(this);
+			recipe.AddRecipe();
+		}
+		
+	}
+}
