630c7dad18d26ce03b8059e3304e1f6ad6fa0715
gyiyg "Dr. Ant, MD" yourselfcola <78702698+mrgyiyg@users.noreply.github.com>
2021-02-17 22:20:40 -0500

    Add files via upload

---
+++ NPCs/Ant.cs
@@ -0,0 +1,80 @@
+using Microsoft.Xna.Framework;
+using MonoMod.Cil;
+using System;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace SmallVanityProject.NPCs
+{
+	internal class AntNPC : ModNPC
+	{
+
+		public override void SetStaticDefaults() {
+			DisplayName.SetDefault("Ant");
+			Main.npcFrameCount[npc.type] = Main.npcFrameCount[NPCID.Scorpion];
+			Main.npcCatchable[npc.type] = true;
+		}
+
+		public override void SetDefaults() {
+			npc.width = 8;
+			npc.height = 5;
+			npc.CloneDefaults(NPCID.Scorpion);
+			npc.catchItem = (short)ModContent.ItemType<AntItem>();
+			npc.lavaImmune = true;
+			npc.friendly = true;
+			aiType = NPCID.Scorpion;
+			animationType = NPCID.Scorpion;
+		}
+
+		public override bool? CanBeHitByItem(Player player, Item item) {
+			return true;
+		}
+
+		public override bool? CanBeHitByProjectile(Projectile projectile) {
+			return true;
+		}
+
+		public override float SpawnChance(NPCSpawnInfo spawnInfo) {
+			return SpawnCondition.OverworldDay.Chance * 0.1f;
+			return SpawnCondition.OverworldDaySandCritter.Chance * 0.1f;
+			return SpawnCondition.Underground.Chance * 0.1f;
+		}
+		
+		public override void OnCatchNPC(Player player, Item item) {
+			item.stack = 1;
+
+				}
+
+		public override void HitEffect(int hitDirection, double damage) {
+			if (npc.life <= 0) {
+				Gore.NewGore(npc.position, npc.velocity, mod.GetGoreSlot("NPCs/AntHalf1"), npc.scale);
+				Gore.NewGore(npc.position, npc.velocity, mod.GetGoreSlot("NPCs/AntHalf2"), npc.scale);
+			}
+		}
+
+	}
+
+	internal class AntItem : ModItem
+	{
+		public override void SetStaticDefaults() {
+			DisplayName.SetDefault("Ant");
+		}
+
+		public override void SetDefaults() {
+			item.useStyle = 1;
+			item.autoReuse = true;
+			item.useTurn = true;
+			item.useAnimation = 15;
+			item.useTime = 10;
+			item.maxStack = 999;
+			item.consumable = true;
+			item.width = 8;
+			item.height = 5;
+			item.noUseGraphic = true;
+			item.CloneDefaults(ItemID.Scorpion);
+			item.bait = 5;
+			item.makeNPC = (short)ModContent.NPCType<AntNPC>();
+		}
+	}
+}
