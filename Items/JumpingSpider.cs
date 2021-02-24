dce1afb2ad758f07fd7d01d6885aee4968b42af2
gyiyg "Dr. Ant, MD" yourselfcola <78702698+mrgyiyg@users.noreply.github.com>
2021-02-23 19:06:36 -0500

    Add files via upload

---
+++ Items/JumpingSpider.cs
@@ -0,0 +1,38 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+using Microsoft.Xna.Framework;
+
+namespace SmallVanityProject.Items
+{
+	[AutoloadEquip(EquipType.Head)]
+	public class JumpingSpider : ModItem
+	{
+		public override void SetStaticDefaults() {
+			base.SetStaticDefaults();
+			DisplayName.SetDefault("Jumping Spider Mask");
+			}
+			
+		public override void SetDefaults() {
+			item.width = 32;
+			item.height = 26;
+			item.rare = ItemRarityID.Blue;
+			item.vanity = true;
+			item.value = Item.sellPrice(0, 1, 50, 0);
+		}
+
+		public override bool DrawHead() {
+			return false;
+		}
+		
+		public override void AddRecipes() {
+			ModRecipe recipe = new ModRecipe(mod);
+			recipe.AddIngredient(ItemID.SpiderFang, 2);
+			recipe.AddIngredient(ItemID.LifeCrystal, 1);			
+			recipe.AddTile(TileID.WorkBenches);
+			recipe.SetResult(this);
+			recipe.AddRecipe();
+		}
+	}
+}
